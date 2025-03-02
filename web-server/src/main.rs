f29b70a83eb8a7150ee6ad80055262ddd9a98a98
destinyFvcker <destinyfunk@163.com>
2025-03-02 15:11:15 +0800

    test: github workflow4

--- web-server/src/main.rs
+++ web-server/src/main.rs
@@ -1,5 +1,7 @@
 use config::CONFIG;
+use ftlog::appender::{FileAppender, Period};
 use poem::{get, handler, listener::TcpListener, Route, Server};
+use time::Duration;
 
 mod config;
 
@@ -14,6 +16,24 @@ fn get_app() -> Route {
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let time_format = time::format_description::parse_owned::<1>(
+        "[year]-[month]-[day] [hour]:[minute]:[second].[subsecond digits:6]",
+    )
+    .unwrap();
+    // TODO 这里应该做一下区分，就是测试分支和部署分支不使用同一个日志等级
+    let _guard = ftlog::builder()
+        .max_log_level(ftlog::LevelFilter::Info)
+        .time_format(time_format)
+        .root(
+            FileAppender::builder()
+                .path("./logs/server.log")
+                .rotate(Period::Day)
+                .expire(Duration::days(7))
+                .build(),
+        )
+        .try_init()
+        .expect("logger build or set failed");
+
     Server::new(TcpListener::bind(format!("0.0.0.0:{}", CONFIG.server.port)))
         .run(get_app())
         .await?;
