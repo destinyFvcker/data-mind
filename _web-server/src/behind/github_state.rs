d2eb7aeab764daf3c03db8cb113dd2962d4f9612
destinyFvcker <destinyfunk@163.com>
2025-04-12 16:12:19 +0800

    feat: init github oauth

---
+++ _web-server/src/behind/github_state.rs
@@ -0,0 +1,85 @@
+//! 刷新/过期 github OAuth 登陆发放的随机字符串state
+use std::{collections::HashMap, sync::Arc, thread, time::Duration};
+
+use dashmap::DashMap;
+use rand::{distr::Alphanumeric, Rng};
+
+/// state将在生成2分钟后过期
+const FRESH_TIME: i8 = 2;
+
+/// 一个dashmap，用于在用户使用github进行鉴权登陆时生成并储存一个随机生成的字符串
+/// 并在稍后用户重定向回服务器时取出进行校验
+pub struct GithubState {
+    /// i8之中记录了剩余过期时间，2分钟过期
+    state_map: DashMap<String, i8>,
+}
+
+impl GithubState {
+    /// 开始后台的dashmap花型行为，将超过期限的dashmap state对删除
+    pub fn begin_processing() -> Arc<Self> {
+        let github_state = Arc::new(GithubState {
+            state_map: DashMap::new(),
+        });
+
+        let arc_ref = github_state.clone();
+        actix_web::rt::spawn(async move {
+            loop {
+                arc_ref.dump_states();
+                actix_web::rt::time::sleep(Duration::from_secs(60)).await;
+                arc_ref.state_map.alter_all(|_, v| v - 1);
+                arc_ref.state_map.retain(|_, value| *value > 0);
+            }
+        });
+
+        github_state
+    }
+
+    pub fn new_state(&self) -> String {
+        let state = Self::gen_state();
+        self.state_map.insert(state.clone(), FRESH_TIME);
+        state
+    }
+
+    pub fn verify_state(&self, input_state: &str) -> bool {
+        if let Some(_) = self.state_map.remove(input_state) {
+            true
+        } else {
+            false
+        }
+    }
+
+    fn gen_state() -> String {
+        rand::rng()
+            .sample_iter(&Alphanumeric)
+            .take(16) // 这里是长度
+            .map(char::from)
+            .collect()
+    }
+
+    #[allow(unused)]
+    fn dump_states(&self) {
+        let current_states = self
+            .state_map
+            .iter()
+            .map(|pair_ref| (pair_ref.key().to_owned(), *pair_ref.value()))
+            .collect::<HashMap<String, i8>>();
+
+        ftlog::debug!("dump current states = {:?}", current_states);
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use rand::{distr::Alphanumeric, Rng};
+
+    #[test]
+    fn test_random_string() {
+        let random_string: String = rand::rng()
+            .sample_iter(&Alphanumeric)
+            .take(16) // 这里是长度
+            .map(char::from)
+            .collect();
+
+        println!("随机字符串: {}", random_string);
+    }
+}
---
+++ _web-server/src/init.rs
@@ -0,0 +1,38 @@
+use std::time::Duration;
+
+use sqlx::{mysql::MySqlPoolOptions, MySql, Pool};
+
+use crate::init_config::InitConfig;
+
+pub struct DbClients {
+    pub clickhouse: clickhouse::Client,
+    pub mysql: Pool<MySql>,
+}
+
+/// 初始化项目数据库
+pub async fn init_db(app_config: &InitConfig) -> DbClients {
+    let clickhouse = clickhouse::Client::default()
+        .with_url(format!(
+            "http://{}:{}",
+            app_config.clickhouse.host, app_config.clickhouse.port
+        ))
+        .with_user(&app_config.clickhouse.user)
+        .with_password(&app_config.clickhouse.password)
+        .with_database(&app_config.clickhouse.database);
+
+    let mysql = MySqlPoolOptions::new()
+        .acquire_timeout(Duration::from_secs(10))
+        .connect(&format!(
+            "mysql://{}:{}@{}:{}/{}",
+            app_config.mysql.user,
+            app_config.mysql.password,
+            app_config.mysql.host,
+            app_config.mysql.port,
+            app_config.mysql.database
+        ))
+        .await
+        .unwrap();
+
+    data_mind::utils::perform_mysql_ddl(&mysql, include_str!("../ddl/auth.sql")).await;
+    DbClients { clickhouse, mysql }
+}
