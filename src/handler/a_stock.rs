dde624ee64aa7c855f8b7ad050538c284309638a
destinyFvcker <destinyfunk@163.com>
2025-05-09 14:46:12 +0800

    feat: add ma-lines api

--- src/handler/a_stock.rs
+++ src/handler/a_stock.rs
@@ -4,17 +4,27 @@ use actix_web::{
     get,
     web::{self, Data, Json},
 };
+use serde::Deserialize;
+use utoipa::IntoParams;
 use utoipa_actix_web::{scope, service_config::ServiceConfig};
 
-use crate::schema::service::a_stock::StockIndividualInfoEm;
+use crate::{
+    repository::MALinesRepo,
+    schema::service::a_stock::{MALines, StockIndividualInfoEm},
+};
 
 pub const API_TAG: &'static str = "A股量化金融数据";
 pub const API_DESC: &'static str = "一组用于获取A股相关金融交易信息的数据接口";
 
 pub fn mount_astock_scope(config: &mut ServiceConfig) {
-    config.service(scope("/astock").service(fetch_stock_individual_info));
+    config.service(
+        scope("/astock")
+            .service(fetch_stock_individual_info)
+            .service(fetch_mas_with_limit),
+    );
 }
 
+/// 获取个股简单信息
 #[utoipa::path(
     tag = API_TAG,
     params(
@@ -34,5 +44,40 @@ async fn fetch_stock_individual_info(
     let data = StockIndividualInfoEm::from_astock_api(&reqwest_client, &stock_id)
         .await
         .map_err(|err| actix_web::error::ErrorNotFound(err))?;
+    Ok(Json(data))
+}
+
+#[derive(Debug, Deserialize, IntoParams)]
+struct MAQuery {
+    /// 需要获取移动平均线的股票代码
+    #[param(example = "603777")]
+    stock_id: String,
+    #[param(example = "30")]
+    /// 限定返回的天数，或者说数据点数量(注意不要超过90天)
+    limit_days: u32,
+}
+
+/// 获取一定时间范围内的移动平均线(MA5/MA10/MA20)数据
+#[utoipa::path(
+    tag = API_TAG,
+    params(
+        MAQuery
+    ),
+    responses(
+        (status = 200, description = "成功获取请求的股票Id对应的时间范围内的移动平均线数据", body = Vec<MALines>)
+    )
+)]
+#[get("/ma_with_limit")]
+async fn fetch_mas_with_limit(
+    ma_query: web::Query<MAQuery>,
+    ch_client: web::Data<clickhouse::Client>,
+) -> actix_web::Result<Json<Vec<MALines>>> {
+    let data: Vec<MALines> =
+        MALinesRepo::fetch_with_limit(&ch_client, &ma_query.stock_id, ma_query.limit_days)
+            .await
+            .map_err(|err| actix_web::error::ErrorInternalServerError(err))?
+            .into_iter()
+            .map(From::from)
+            .collect();
     Ok(Json(data))
 }
--- src/schema/service/a_stock.rs
+++ src/schema/service/a_stock.rs
@@ -1,17 +1,55 @@
 //! A股相关数据dto实体定义
 
 use chrono::NaiveDate;
-use clickhouse::Row;
 use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
 
+use crate::repository::MALinesRepo;
 pub use crate::schema::akshare::StockIndividualInfoEm;
 
 /// 移动平均线数据(MA5/MA10/MA20)
-#[derive(Debug, Serialize, Deserialize, Row, ToSchema)]
-struct MALines {
-    date: NaiveDate,
-    ma5: Option<f64>,
-    ma10: Option<f64>,
-    ma20: Option<f64>,
+#[derive(Debug, Serialize, Deserialize, ToSchema)]
+pub struct MALines {
+    /// 数据点日期，格式为YYYY-MM-DD
+    #[schema(example = "2025-05-08")]
+    pub date: NaiveDate,
+    /// 数据点日期对应的MA5值，注意单位(元)
+    #[schema(example = 13.29)]
+    pub ma5: Option<f64>,
+    /// 数据点日期对应的MA10值。注意单位(元)
+    #[schema(example = 13.481)]
+    pub ma10: Option<f64>,
+    /// 数据点日期对应的MA20值，注意单位(元)
+    #[schema(example = 13.8955)]
+    pub ma20: Option<f64>,
+}
+
+impl From<MALinesRepo> for MALines {
+    fn from(value: MALinesRepo) -> Self {
+        Self {
+            date: value.date,
+            ma5: value.ma5,
+            ma10: value.ma10,
+            ma20: value.ma20,
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::utils::TEST_CH_CLIENT;
+
+    use super::*;
+
+    #[tokio::test]
+    async fn test_fetch_malines() {
+        let data: Vec<MALines> = MALinesRepo::fetch_with_limit(&TEST_CH_CLIENT, "603777", 90)
+            .await
+            .unwrap()
+            .into_iter()
+            .map(From::from)
+            .collect();
+
+        println!("{}", serde_json::to_string_pretty(&data).unwrap());
+    }
 }
