18e1108b5d8a27b272c872e118c2972786feb2d4
destinyFvcker <destinyfunk@163.com>
2025-05-14 00:52:54 +0800

    feat: mount quant data to web-server

--- _web-server/src/main.rs
+++ _web-server/src/main.rs
@@ -4,13 +4,19 @@ use actix_web::{
     web::Data,
     App, HttpServer,
 };
+use data_mind::handler::{a_stock, indicator, news};
 use ftlog::appender::{FileAppender, Period};
 use handler::{auth::jwt_mw::JwtAuthGuard, *};
+use init::DbClients;
 use std::{env, net::Ipv4Addr, sync::Arc};
 use time::Duration;
-use utoipa::OpenApi;
+use utoipa::{
+    openapi::security::{HttpAuthScheme, HttpBuilder, SecurityScheme},
+    Modify, OpenApi,
+};
 use utoipa_actix_web::AppExt;
 use utoipa_scalar::{Scalar, Servable};
+use utoipa_swagger_ui::SwaggerUi;
 
 mod background;
 mod handler;
@@ -44,7 +50,7 @@ async fn main() {
 
     // ----------------------------------------- ⬇️ some init operation
     // init db
-    let db_clietns = Arc::new(init::init_db(&app_config).await);
+    let DbClients { clickhouse, mysql } = init::init_db(&app_config).await;
     // init github_state refresh on main thread
     let github_state = background::github_state::GithubStateCache::begin_processing();
     // init reqwest client
@@ -54,10 +60,35 @@ async fn main() {
     #[derive(OpenApi)]
     #[openapi(
         tags(
-            (name = auth::API_TAG, description = auth::API_DESC)
-        )
+            (name = auth::API_TAG, description = auth::API_DESC),
+            (name = indicator::API_TAG, description = indicator::API_DESC),
+            (name = news::API_TAG, description = news::API_DESC),
+            (name = a_stock::API_TAG, description = a_stock::API_DESC)
+        ),
+        servers(
+            (url = "http://localhost:8800", description = "本地测试环境"),
+            (url = "https://www.destinyfvcker.cn/data-mind", description = "线上部署环境")
+        ),
+        modifiers(&SecurityAddon)
     )]
     struct ApiDoc;
+
+    struct SecurityAddon;
+
+    impl Modify for SecurityAddon {
+        fn modify(&self, openapi: &mut utoipa::openapi::OpenApi) {
+            let components = openapi.components.as_mut().unwrap(); // we can unwrap safely since there already is components registered.
+            components.add_security_scheme(
+                "bearer",
+                SecurityScheme::Http(
+                    HttpBuilder::new()
+                        .scheme(HttpAuthScheme::Bearer)
+                        .bearer_format("JWT")
+                        .build(),
+                ),
+            )
+        }
+    }
 
     ftlog::info!("Data Mind web server stated!");
     let shared_config = app_config.clone();
@@ -65,11 +96,13 @@ async fn main() {
         let mut app = App::new()
             .into_utoipa_app()
             .openapi(ApiDoc::openapi())
-            .app_data(Data::from(db_clietns.clone()))
+            .app_data(Data::new(clickhouse.clone()))
+            .app_data(Data::new(mysql.clone()))
             .app_data(Data::new(reqwest_client.clone()))
             .app_data(Data::from(shared_config.clone()))
             .service(
                 utoipa_actix_web::scope("/api")
+                    .configure(handler::quant_data::config())
                     .wrap(JwtAuthGuard::new(shared_config.jwt_secret_key.clone())),
             )
             .service(
@@ -77,6 +110,9 @@ async fn main() {
                     .configure(handler::auth::config(Data::from(github_state.clone()))),
             )
             .openapi_service(|api| Scalar::with_url("/scalar-doc", api))
+            .openapi_service(|api| {
+                SwaggerUi::new("/swagger-ui/{_:.*}").url("/api-docs/openapi.json", api)
+            })
             .into_app();
 
         app = if local_dev == "1" {
